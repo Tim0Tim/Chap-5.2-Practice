73e13424853dd0a5f12466150ac8b3f127f22cc9
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-03-04 11:11:45 -0700

    Add project files.

---
+++ 5.2 Practice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5._2_Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            int passes = 0;
+            int failures = 0;
+            int studentCounter = 1;
+
+            while (studentCounter <= 10)
+            {
+                Console.Write("Enter resulet (1 = pass, 2 = fail): ");
+                int result = int.Parse(Console.ReadLine());
+
+                if (result == 1)
+                {
+                    passes = passes + 1;
+                }
+                else
+                {
+                    failures = failures + 1;
+                }
+
+                studentCounter = studentCounter + 1;
+            }
+
+            Console.WriteLine($"Passed: {passes}\nFailed: {failures}");
+
+            if (passes > 8)
+            {
+                Console.WriteLine("Bonus to instructor!");
+            }
+        }
+    }
+}
